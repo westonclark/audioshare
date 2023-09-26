e247e75f6d12d45b2383ee411d3d351417a8549c
Weston Clark <westoclark@gmail.com>
2023-09-26 16:41:39 -0700

    remove images

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank"></a>
-        <a href="https://react.dev" target="_blank"></a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
