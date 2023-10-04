3ec56e2cd7840eaa7aff3b781d5db0311c0ee6ff
Weston Clark <westoclark@gmail.com>
2023-10-03 18:23:01 -0700

    Add styling to Login page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <>
-      <h1>Welcome to AudioShare</h1>
-    </>
+    <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0 font-inter">
+      <div className="md:flex md:justify-between md:gap-4 md:py-36">
+        <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:gap-8">
+          <div>
+            <h1 className="text-4xl font-bold tracking-tight sm:text-5xl ">Audioshare.com</h1>
+            <h2 className="mt-3 text-lg font-medium tracking-tight sm:text-xl">Send audio files and recieve notes all in one place</h2>
+            {/* ADD DEMO GIF HERE */}
+          </div>
+        </header>
+        <main className="flex  gap-8 h-min">
+          <a href="/login">Login</a>
+          <a href="/signup">Signup</a>
+        </main>
+      </div>
+    </div>
   );
 }
--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -2,5 +2,26 @@ import React from 'react';
 import { useState } from 'react';
 
 export default function LogIn() {
-  return <h1>Login In Page</h1>;
+  return (
+    <div className="flex justify-center mx-auto min-h-screen max-w-screen-xl px-6 py-40 font-sans md:px-12 md:py-20 lg:px-24 lg:py-60 font-inter">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-4xl font-bold tracking-tight sm:text-5xl text-center">Audioshare.com</h1>
+        <form className="flex flex-col gap-4">
+          <input type="text" placeholder="email"></input>
+          <input type="text" placeholder="password"></input>
+          <button type="submit" className="border border-black rounded bg-slate-600 text-slate-100">
+            Log In
+          </button>
+        </form>
+
+        <div className="flex items-center gap-4">
+          <hr className="h-px border-0  bg-slate-600 w-full"></hr> <p className="text-center">or</p>
+          <hr className="h-px border-0 bg-slate-600 w-full"></hr>
+        </div>
+        <a className="border border-black rounded bg-slate-300 text-center" href="/signup">
+          Sign Up
+        </a>
+      </div>
+    </div>
+  );
 }
